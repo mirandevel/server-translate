2c03a571b85e58e5847a214aac302bfcaff3df34
miranda <miranda.devel@gmail.com>
2021-10-10 22:14:23 -0400

    with graphql

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
+/*
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
@@ -10,6 +11,28 @@ module.exports = ({ env }) => ({
       options: {
         useNullAsDefault: true,
       },
+    },
+  },
+});
+*/
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'ec2-18-214-214-252.compute-1.amazonaws.com'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'd6rnmpgvjbo4o4'),
+        username: env('DATABASE_USERNAME', 'eumsmtaspkgyni'),
+        password: env('DATABASE_PASSWORD', '32fb6fcb4ae0d9c324803cf68380cf03fe25b127bbbf649086b6824214bda30d'),
+        schema: env('DATABASE_SCHEMA', 'public'), // Not Required
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        },
+      },
+      options: {},
     },
   },
 });
