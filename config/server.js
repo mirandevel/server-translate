a5f83bf099667d558ab361efdc8b4ccc39c6321f
miranda <miranda.devel@gmail.com>
2021-10-10 21:01:54 -0400

    with tailwind

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2bf7e755d68c27d86740ec48ae3b644d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fce0084a-b87b-4f28-94ed-410c5ee91267'
+};
